66ab417b568dc0df17d9ded585f3bcbca70c41c6
kubapoke <q.paczka@gmail.com>
2026-09-10 20:24:06 +0200

    Added test_opcode as default app

--- src/app/app.rs
+++ src/app/app.rs
@@ -30,7 +30,7 @@ impl App {
         let mut emulator = Emulator::new(Some(display_sender), Some(input_receiver));
 
         let handle = thread::spawn(move || {
-            emulator.load_program(Path::new("./programs/IBM Logo.ch8")); // TODO: Add proper program loading
+            emulator.load_program(Path::new("./programs/test_opcode.ch8")); // TODO: Add proper program loading
             emulator.begin_execution();
         });
 
